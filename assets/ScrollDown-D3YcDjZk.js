9d857adcba4a4c39084f7cf65c3a4aedcf0412a8
yichao <yichao.wang.work@gmail.com>
2024-02-05 10:45:55 +0800

    feat: refine details

--- assets/ScrollDown-D3YcDjZk.js
+++ assets/ScrollDown-D3YcDjZk.js
@@ -1,4 +1,4 @@
-import{r as Un,j as $n}from"./index-5PGLIXRp.js";function er(a){if(a===void 0)throw new ReferenceError("this hasn't been initialised - super() hasn't been called");return a}function Bo(a,e){a.prototype=Object.create(e.prototype),a.prototype.constructor=a,a.__proto__=e}/*!
+import{r as Un,j as $n}from"./index-j8UJxU13.js";function er(a){if(a===void 0)throw new ReferenceError("this hasn't been initialised - super() hasn't been called");return a}function Bo(a,e){a.prototype=Object.create(e.prototype),a.prototype.constructor=a,a.__proto__=e}/*!
  * GSAP 3.12.2
  * https://greensock.com
  *
